86ea67baa1eadca03bd31641a075b6f21a7a8b31
Qamar.cpp <hassanabnas876@gmail.com>
2026-04-29 23:22:57 +0500

    Add my15 print name program

---
+++ my15.js
@@ -0,0 +1,8 @@
+function printmyname(name){
+    "use strict";
+    const SENTENCE = name + "very cool";
+    for(let i = 0; i < name.length; i+=2){
+        return SENTENCE;
+    }
+}
+console.log(printmyname("code with luna "))
